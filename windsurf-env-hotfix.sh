efa427893c3dab79ee4ffeeb98711f89c1393d9c
onpurpose <stylerforbes@gmail.com>
2025-08-29 07:35:44 -0400

    Create windsurf-env-hotfix.sh

---
+++ windsurf-env-hotfix.sh
@@ -0,0 +1,5 @@
+chmod +x windsurf-env-hotfix.sh
+POSTGRES_URL=postgres://USER:PASSWORD@HOST:PORT/DATABASE
+NEXT_PUBLIC_API_URL=https://api.example.com
+ANOTHER_ENV_VAR=value
+./windsurf-env-hotfix.sh
