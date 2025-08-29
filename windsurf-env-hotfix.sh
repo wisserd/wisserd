faefd1686ef48e1b3926c135db83db4a7d171099
onpurpose <stylerforbes@gmail.com>
2025-08-29 07:41:45 -0400

    Update windsurf-env-hotfix.sh

--- windsurf-env-hotfix.sh
+++ windsurf-env-hotfix.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-
 # This script applies a hotfix to the windsurf environment
 
-# Check if the user has root privileges
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
-# Apply the hotfix
 echo "Applying windsurf environment hotfix..."
-sed -i 's/old_config/new_config/g' /path/to/config/file
 
-echo "Hotfix applied successfully"
+# Add your hotfix commands here
+# For example:
+# sed -i 's/old_value/new_value/' /path/to/config/file
+
+echo "Hotfix applied successfully."
